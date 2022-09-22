3da47219300b12c63397e2f2bf56e87f35ea0f10
rafaapcode <rafaap2003@gmail.com>
2022-09-22 15:59:25 -0300

    Reset

--- server.js
+++
@@ -1,7 +0,0 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-import app from './app';
-
-app.listen(process.env.PORT, () => console.log(`Running on port ${process.env.PORT}`));
--- src/database/index.js
+++
@@ -1,9 +0,0 @@
-import Sequelize from 'sequelize';
-import dataConfig from '../config/database.js';
-import Employee from '../models/Employee.js';
-
-const models = [Employee];
-
-const connection = new Sequelize(dataConfig);
-
-models.forEach(model => model.init(connection));
